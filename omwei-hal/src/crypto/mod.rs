731c06883d964ef2650119809c294beae8a601bc
Witness of Agape <creator@omwei.tech>
2026-07-23 20:52:42 +0200

    Remove omwei-hal directory (hardware abstraction layer)

--- omwei-hal/src/crypto/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Cryptographic hardware drivers for OMWEI Equinibrium SoC
-//! 
-//! This module provides drivers for the Hardware Crypto Accelerator (HCA)
-//! peripherals, including the True Random Number Generator (TRNG) and
-//! AES-256 encryption engine.
-
-pub mod trng;
-
-pub use trng::Trng;
--- omwei-hal/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![no_std]
-
-pub mod registers;
-pub mod sync;
-pub mod semantic;
-pub mod uart;
-pub mod interrupts;
-pub mod crypto;
